6f90efe1e027e3633f6b015a53d3c9a1a7fdf576
ingridwy <bluebear589@gmail.com>
2020-09-24 18:32:42 +0100

    remove comment

--- alpha.rb
+++ alpha.rb
@@ -36,24 +36,3 @@ def convert(sentence,number)
   return word.join
 end
 
-#new_index = split_word.map { |alphabet|
-
-    #alphabet_list = "
-    #ABCDEFGHIJKLM
-    #NOPQRSTUVWXYZ".split(//)
-
-    #alphabet.index
- # }
-#abcdefghijklmnopqrstuvwxyz
-#NOPQRSTUVWXYZABCDEFGHIJKLM
-#You are required to create a basic program which implements the basis of a ROT-13 algorithm,
-#taking a string as an input and replacing each letter with the 13th letter after it. For example if I input "hello world" it should return "uryyb jbeyq".
-#You should be able to customize the rotation value. IE I should be able to change 13 to any number [ n ] between 1 and 25 and it will replace each letter with the nth letter after it.
-#It should not change punctuation or numbers
-#It should be case sensitive
-#(BONUS) build a client side interface for the program where the user can enter the string and rotation value and then see the returned output
-#Test Cases:
-
-#Rotation: 13, User Input: "Hello World", Expected Output: "Uryyb Jbeyq"
-#Rotation: 13, User Input: "The Number 16.", Expected Output: "Gur Ahzore 16."
-#Rotation: 19, User Input: "Hello World!", Expected Output: "Axeeh Phkew!"
